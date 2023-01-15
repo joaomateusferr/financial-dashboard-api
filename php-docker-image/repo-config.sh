69649208ff5c80d8115465cce7fd78881e976d76
Joao Ferreira <joaomateusferr@gmail.com>
2023-01-15 11:11:35 -0300

    add project docker image

---
+++ php-docker-image/repo-config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+GIT_REPOSITORY='https://github.com/joaomateusferr/financial-dashboard.git'
+
+APACHE_SITE_PATH='/var/www/html/'
+DEFAULT_APACHE_SITES_PATH='/etc/apache2/sites-available/000-default.conf'
+
+REPO_FOLDER_NAME=$(basename -s .git $GIT_REPOSITORY)
+REPO_FOLDER="$APACHE_SITE_PATH$REPO_FOLDER_NAME"
+
+git -C $APACHE_SITE_PATH clone $GIT_REPOSITORY
+sed -i "s|/var/www/html|$REPO_FOLDER/public|g" $DEFAULT_APACHE_SITES_PATH
