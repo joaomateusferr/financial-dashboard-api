ef1f77ad62a097885604c82ae626326b7ef8010b
Joao Ferreira <joaomateusferr@gmail.com>
2023-11-30 01:44:51 -0300

    create api base

---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+$Routes = [
+    'GET' => [
+        'ExchangeTradedAssets' => ['Arguments' => [] , 'Midwares' => [], 'Controler' => 'Controler', 'Method' => 'Method']
+    ],
+    'POST' => [
+        'rota/ARG/aaa', 'controler', 'method', 'midwere'
+    ],
+];
--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
-	header("Location: components/pages/home.php");
+
+	require_once dirname(__FILE__)."/../config/routes.php";
+
+	header("Content-type: application/json; charset=UTF-8");
+
+	$Tokens = explode("/", $_SERVER["REQUEST_URI"]);
+
+	if(!isset($Routes[$_SERVER["REQUEST_METHOD"]][$Tokens[0]])){
+		http_response_code(404);
+    	exit;
+	}
+
+	var_dump($Tokens);exit;
+
+
+	var_dump($_SERVER["REQUEST_METHOD"]);exit;
+	var_dump($Routes);exit;
+	//header("Location: components/pages/home.php");
 ?>
