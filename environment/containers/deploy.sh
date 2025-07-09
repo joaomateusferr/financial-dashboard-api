493ed2b56eb23aacac4fbe97c8beba8ea23906ef
Joao Ferreira <joaomateusferr@gmail.com>
2025-07-09 18:20:43 -0300

    Add valkey environment deploy script

--- environment/containers/deploy.sh
+++ environment/containers/deploy.sh
@@ -6,3 +6,6 @@ docker run --name mariadb -d -e MYSQL_ROOT_PASSWORD=$PWD -e MYSQL_DATABASE=syste
 docker run --name phpmyadmin -d --link mariadb:db -p 8080:80 -e PMA_HOST=db -e PMA_PORT=3306 phpmyadmin:latest
 
 echo "Database password is: $PWD"
+
+docker run -d --name=valkey --restart=always -p 6379:6379 valkey/valkey:latest valkey-server --save "" --appendonly no
+docker exec -it valkey valkey-cli PING
