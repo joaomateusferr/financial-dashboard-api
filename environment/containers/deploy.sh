978f680ce2e3621f7e8ff734d1fcfbdfb1474f7b
Joao Ferreira <joaomateusferr@gmail.com>
2025-07-13 11:05:47 -0300

    Fix valkey deploy timing issue

--- environment/containers/deploy.sh
+++ environment/containers/deploy.sh
@@ -8,4 +8,5 @@ docker run --name phpmyadmin -d --link mariadb:db -p 8080:80 -e PMA_HOST=db -e P
 echo "Database password is: $PWD"
 
 docker run -d --name=valkey --restart=always -p 6379:6379 valkey/valkey:latest valkey-server --save "" --appendonly no
+sleep 2
 docker exec -it valkey valkey-cli PING
