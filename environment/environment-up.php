e966bfca05e8a8f3fcb8460e309aef3429606a12
Joao Ferreira <joaomateusferr@gmail.com>
2025-11-19 14:24:40 -0300

    Add script

--- environment/environment-up.php
+++ environment/environment-up.php
@@ -1,9 +1,18 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Services\SharedMemory;
+use App\Constants\KeysConstants;
+
 $Mode = isset($argv[1]) ? $argv[1] : 'CI';
 
+$SupportedModes = ['CI', 'DEV', 'DB_ONLY'];
 $DatabaseNetwork = 'network-db';
 $DatabaseNetworkAlias = 'db';
+
+if(!in_array($Mode, $SupportedModes))
+    exit(1);    //Mode not supported
 
 $Output = [];
 $ResultCode = 0;
@@ -11,7 +20,7 @@ $ResultCode = 0;
 exec("openssl rand -base64 45 | tr -dc 'A-Za-z0-9-().!@?#,/;+' | head -c30", $Output, $ResultCode); //Creating a cryptographically strong random password
 
 if(!empty($ResultCode) || !isset($Output[0]))
-    exit(1);    //Unable to generate database password
+    exit(2);    //Unable to generate database password
 
 $DatabasePassword = trim($Output[0]);
 
@@ -21,7 +30,7 @@ $ResultCode = 0;
 exec("hostname -I | awk '{print $2}'", $Output, $ResultCode);
 
 if(!empty($ResultCode) || !isset($Output[0]))
-    exit(2);    //Unable to get database host
+    exit(3);    //Unable to get database host
 
 $DatabaseHost = empty($Output[0]) ? $DatabaseNetworkAlias : trim($Output[0]);
 
@@ -31,7 +40,7 @@ $ResultCode = 0;
 exec("docker network create $DatabaseNetwork", $Output, $ResultCode);
 
 if(!empty($ResultCode))
-    exit(3);    //Unable to create database network
+    exit(4);    //Unable to create database network
 
 $Output = [];
 $ResultCode = 0;
@@ -39,4 +48,87 @@ $ResultCode = 0;
 exec("docker run --name mariadb -d --network $DatabaseNetwork --network-alias $DatabaseNetworkAlias -e MYSQL_ROOT_PASSWORD=$DatabasePassword -e MYSQL_DATABASE=system -p 3306:3306 mariadb:latest", $Output, $ResultCode);
 
 if(!empty($ResultCode))
-    exit(4);    //Unable to run mariadb container
+    exit(5);    //Unable to run mariadb container
+
+if($Mode != 'CI'){
+
+    echo "Database Password: $DatabasePassword\n";
+
+    $Output = [];
+    $ResultCode = 0;
+
+    exec("docker run --name phpmyadmin -d --network $DatabaseNetwork -p 8080:80 -e PMA_HOST=$DatabaseNetworkAlias -e PMA_PORT=3306 phpmyadmin:latest", $Output, $ResultCode);
+
+    if(!empty($ResultCode))
+        exit(6);    //Unable to run phpmyadmin container
+
+}
+
+if($Mode == 'DB_ONLY')
+    exit(0);
+
+$ServersList = [
+
+    'kernel' => [
+        'Host' => $DatabaseHost,
+        'Port' => 3306,
+        'HasSSL' => false,
+    ],
+
+    'common-information' => [
+        'Host' => $DatabaseHost,
+        'Port' => 3306,
+        'HasSSL' => false,
+    ],
+
+    'customers-server-1' => [
+        'Host' => $DatabaseHost,
+        'Port' => 3306,
+        'HasSSL' => false,
+    ],
+];
+
+$DatabaseCredentials = ['User' => 'root', 'Password' => $DatabasePassword];
+
+$Output = [];
+$ResultCode = 0;
+
+exec("openssl rand -base64 32", $Output, $ResultCode); //Creating a cryptographically strong random key
+
+if(!empty($ResultCode) || !isset($Output[0]))
+    exit(7);    //Unable to generate random key
+
+try{
+
+    $SharedMemory = new SharedMemory(KeysConstants::getServersList());
+    $SharedMemory->write($ServersList);
+
+} catch (Exception $Ex) {
+
+    exit(8); //Unable to load database credentials
+
+}
+
+try{
+
+    $SharedMemory = new SharedMemory(KeysConstants::getDatabaseCredentials());
+    $SharedMemory->write($DatabaseCredentials);
+
+} catch (Exception $Ex) {
+
+    exit(9);   //Unable to load servers list
+
+}
+
+$JwtCredentials = ['Key' => trim($Output[0])];
+
+try{
+
+    $SharedMemory = new SharedMemory(KeysConstants::getJwtCredentials());
+    $SharedMemory->write($JwtCredentials);
+
+} catch (Exception $Ex) {
+
+    exit(10);   //Unable to load jwt credentials
+
+}
