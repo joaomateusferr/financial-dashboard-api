3944adec079829835ce752102353e0d5524c767c
Joao Ferreira <joaomateusferr@gmail.com>
2025-11-29 19:26:41 -0300

    Change http response test to static to use set up before class to make the request only once per test class

--- tests/integration/PingTest.php
+++ tests/integration/PingTest.php
@@ -6,26 +6,26 @@ use TestDependencies\HTTP\HTTPResponseTest;
 
 final class PingTest extends HTTPResponseTest {
 
-    protected function setUp() : void {
+    public static function setUpBeforeClass() : void {
 
         $Curl = curl_init();
         $Url = "http://localhost:8888/ping";
         curl_setopt($Curl, CURLOPT_URL, $Url);
         curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
-        $this->Response = curl_exec($Curl);
-        $this->HttpCode = curl_getinfo($Curl, CURLINFO_HTTP_CODE);
+        self::$Response = curl_exec($Curl);
+        self::$HttpCode = curl_getinfo($Curl, CURLINFO_HTTP_CODE);
 
     }
 
     public function testResponseHttpCodeIs200(): void {
 
-        $this->assertSame($this->HttpCode, 200);
+        $this->assertSame(self::$HttpCode, 200);
 
     }
 
     public function testResponseDefaultExpectedValues(): void {
 
-        $ResponseArray = json_decode($this->Response,true);
+        $ResponseArray = json_decode(self::$Response,true);
         $this->assertSame($ResponseArray, ['result' => ['pong']]);
 
     }
