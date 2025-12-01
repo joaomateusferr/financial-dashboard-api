b9796c0e11d84246d723bb1244877b6b3e592e30
Joao Ferreira <joaomateusferr@gmail.com>
2025-12-01 08:23:42 -0300

    Add create already registered user test.php

---
+++ tests/sequential/user/CreateAlreadyRegisteredUserTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use TestDependencies\HTTP\HTTPResponseTest;
+
+final class CreateAlreadyRegisteredUserTest extends HTTPResponseTest {
+
+    public static function setUpBeforeClass(): void {
+
+        $Curl = curl_init();
+        $Url = "http://localhost:8888//user/create";
+        curl_setopt($Curl, CURLOPT_URL, $Url);
+        curl_setopt($Curl, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+        curl_setopt($Curl, CURLOPT_POST, true);
+        curl_setopt($Curl, CURLOPT_POSTFIELDS, json_encode(['Email' => 'joao.ferreira@gmail.com', 'Password' => 'Test1401!']));
+        curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
+        self::$Response = curl_exec($Curl);
+        self::$HttpCode = curl_getinfo($Curl, CURLINFO_HTTP_CODE);
+
+    }
+
+    public function testResponseHttpCodeIs409(): void {
+
+        $this->assertSame(self::$HttpCode, 409);
+
+    }
+
+    public function testResponseDefaultExpectedMessage(): void {
+
+        $ResponseArray = json_decode(self::$Response,true);
+        $this->assertSame($ResponseArray['result']['message'], 'User already registered!');
+
+    }
+
+}
