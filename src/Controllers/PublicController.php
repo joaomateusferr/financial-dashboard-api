b98873650026662932d6d5f8bd993d2a68ccd2d8
Joao Ferreira <joaomateusferr@gmail.com>
2025-12-05 06:17:27 -0300

    Add base controller to public controller

--- src/Controllers/PublicController.php
+++ src/Controllers/PublicController.php
@@ -5,25 +5,11 @@ namespace App\Controllers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Helpers\RequestHelper;
-use App\Helpers\ResponseHelper;
-
-class PublicController {
+class PublicController extends BaseController {
 
     public function ping(Request $Request, Response $Response) {
 
-        $Data = ["pong"];
-
-        $Response->getBody()->write(ResponseHelper::format($Data));
-        return $Response->withHeader('Content-Type', ResponseHelper::getDefaultContentType())->withStatus(200);
-
-    }
-
-    public function login(Request $Request, Response $Response) {
-
-        $Data = RequestHelper::formatBody($Request->getBody()->getContents());
-
-        $Response->getBody()->write(ResponseHelper::format($Data));
-        return $Response->withHeader('Content-Type', ResponseHelper::getDefaultContentType())->withStatus(200);
+        return self::buildResponse($Response,["pong"]);
 
     }
 
