8fd941ab8c25a9e383ba807cf0d441b387263a0b
Joao Ferreira <joaomateusferr@gmail.com>
2025-12-10 07:49:55 -0300

    Add api base to http not found middleware

--- src/Middlewares/HttpNotFoundMiddleware.php
+++ src/Middlewares/HttpNotFoundMiddleware.php
@@ -5,10 +5,9 @@ namespace App\Middlewares;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpNotFoundException;
-use Slim\Psr7\Factory\ResponseFactory;
-use App\Helpers\ResponseHelper;
+use App\Services\ApiBase;
 
-class HttpNotFoundMiddleware {
+class HttpNotFoundMiddleware extends ApiBase {
 
     public function __invoke(Request $Request, $Handler): Response {
 
@@ -18,10 +17,8 @@ class HttpNotFoundMiddleware {
 
         } catch (HttpNotFoundException $Exception) {
 
-            $ResponseFactory = new ResponseFactory();
-            $Response = $ResponseFactory->createResponse(404);
-            $Response->getBody()->write(ResponseHelper::format('Not Found', true));
-            return $Response->withHeader('Content-Type', ResponseHelper::getDefaultContentType());
+            return self::buildResponseFromFactory(['Not Found'], 404, true);
+
         }
     }
 }
