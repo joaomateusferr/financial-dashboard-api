ce7d4e929d45efaed89c4efda0c51b9fd1519c67
Joao Ferreira <joaomateusferr@gmail.com>
2025-12-19 15:45:37 -0300

    New route helper, add require authentication method

--- src/Helpers/RouteHelper.php
+++ src/Helpers/RouteHelper.php
@@ -4,20 +4,36 @@ namespace App\Helpers;
 
 class RouteHelper {
 
-    private const NoAuthRoots = [
-        'ping',
-        'user'
+    private const NoAuth = [
+        'ping' => ['GET'],
+        'user'=>['POST'],
+        'session' => ['POST'],
     ];
 
-    public static function getNoAuthRoots() : array {
+    private static function getNoAuth() : array {
 
-        return self::NoAuthRoots;
+        return self::NoAuth;
 
     }
 
     public static function getRouteRoot(string $Route) : string {
 
         return explode('/', $Route)[1];
+
+    }
+
+    public static function requireAuthentication(string $Route, string $Method) : bool {
+
+        $RouteRoot = self::getRouteRoot($Route);
+        $NoAuth = self::getNoAuth();
+
+        if(!isset($NoAuth[$RouteRoot]))
+            return true;
+
+        if(in_array($Method, $NoAuth[$RouteRoot]))
+            return false;
+
+        return true;
 
     }
 
