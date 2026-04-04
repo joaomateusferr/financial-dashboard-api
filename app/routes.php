975dc6f3b9196b6ee0b6d5952e7ca7ed2e86c5a2
Joao Ferreira <joaomateusferr@gmail.com>
2026-04-04 09:26:19 -0300

    Add get asset qualification to api

--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,7 @@ use App\Controllers\PublicController;
 use App\Controllers\UserController;
 use App\Controllers\SessionController;
 use App\Controllers\ExchangeTradedAssetsController;
+use App\Controllers\AssetQualificationController;
 use App\Controllers\AssetTypeController;
 use App\Controllers\ExchangeController;
 use App\Middlewares\AuthMiddleware;
@@ -19,6 +20,7 @@ return function (App $app) {
     $app->post('/session', [SessionController::class, 'set']);
     $app->delete('/session', [SessionController::class, 'delete'])->add(new AuthMiddleware());
     $app->post('/common-information/exchange-traded-assets', [ExchangeTradedAssetsController::class, 'create'])->add(new AuthMiddleware());
+    $app->get('/common-information/asset-qualification', [AssetQualificationController::class, 'get']);
     $app->get('/common-information/asset-type', [AssetTypeController::class, 'get']);
     $app->get('/common-information/exchange', [ExchangeController::class, 'get']);
 
