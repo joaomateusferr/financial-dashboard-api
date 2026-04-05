1f50ce296f522672d9f927be4f9a22729d3bb0e6
Joao Ferreira <joaomateusferr@gmail.com>
2026-04-05 19:29:44 -0300

    Change exchange traded assets post limit

--- src/Constants/ApiLimitsConstants.php
+++ src/Constants/ApiLimitsConstants.php
@@ -4,7 +4,7 @@ namespace App\Constants;
 
 class ApiLimitsConstants {
 
-    private const ExchangeTradedAssetsPost = 25;
+    private const ExchangeTradedAssetsPost = 2;
 
     public static function getExchangeTradedAssetsPost() : int {
         return self::ExchangeTradedAssetsPost;
