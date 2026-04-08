fa35c71ab5d641ff9d67e51fe940f3d359c58b72
Joao Ferreira <joaomateusferr@gmail.com>
2026-04-08 14:02:51 -0300

    Add get common information asset type endpoit tests

---
+++ tests/integration/GetCommonInformationAssetTypeEmptyBodyTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use TestDependencies\HTTP\HTTPResponseTest;
+
+final class GetCommonInformationAssetTypeEmptyBodyTest extends HTTPResponseTest {
+
+    public static function setUpBeforeClass(): void {
+
+        $Curl = curl_init();
+        $Url = "http://localhost:8888/common-information/asset-type";
+        curl_setopt($Curl, CURLOPT_URL, $Url);
+        curl_setopt($Curl, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+        curl_setopt($Curl, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($Curl, CURLOPT_POSTFIELDS, json_encode([]));
+        curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
+        self::$Response = curl_exec($Curl);
+        self::$HttpCode = curl_getinfo($Curl, CURLINFO_HTTP_CODE);
+
+    }
+
+    public function testResponseHttpCodeIs400(): void {
+
+        $this->assertSame(self::$HttpCode, 400);
+
+    }
+
+    public function testResponseDefaultExpectedValues(): void {
+
+        $ResponseArray = json_decode(self::$Response,true);
+        $this->assertSame($ResponseArray['error'], true);
+        $this->assertSame($ResponseArray['result'][0], 'Asset types are required!');
+
+    }
+
+}
