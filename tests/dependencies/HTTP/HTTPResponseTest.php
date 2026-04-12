2c4a44244fa2ef833971385df7f537e556d30c62
Joao Ferreira <joaomateusferr@gmail.com>
2026-04-12 13:48:37 -0300

    Add login and logout to http test dependencies

--- tests/dependencies/HTTP/HTTPResponseTest.php
+++ tests/dependencies/HTTP/HTTPResponseTest.php
@@ -15,6 +15,43 @@ abstract class HTTPResponseTest extends TestCase {
 
     }
 
+    public static function login(string $User, string $Password) : ?string {
+
+        $Cookie = null;
+        $Options = [ 'http' => ['user_agent' => 'script','header'  => "Content-type: application/json",'method'  => 'POST', 'content' => json_encode(['Email' => $User, 'Password' => $Password])]];
+        $Result = @file_get_contents(self::getApiBase().'/session', false, stream_context_create($Options));
+
+        if(!empty($Result)){
+
+            foreach($http_response_header as $ResponseHeaderLine){
+
+                if (preg_match('/^Set-Cookie:\s*([^;]+)/i', $ResponseHeaderLine, $Matches)) {
+
+                    $Cookie = $Matches[1];
+                    break;
+
+                }
+
+            }
+
+        }
+
+        return $Cookie;
+
+    }
+
+    public static function logout(string $Cookie) : bool {
+
+        $Options = [ 'http' => ['header'  => "Cookie: $Cookie\r\n",'method'  => 'DELETE']];
+        $Result = @file_get_contents(self::getApiBase().'/session', false, stream_context_create($Options));
+
+        if(!empty($Result))
+            return true;
+
+        return false;
+
+    }
+
     public function testResponseIsNotBool(): void {
 
         $this->assertFalse(is_bool(self::$Response));
