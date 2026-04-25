b92b76f596fae7415ee5a083161370b458b6ffc2
Joao Ferreira <joaomateusferr@gmail.com>
2026-04-25 09:10:15 -0300

    Add get common information exchange traded asset test

---
+++ tests/integration/common-information/exchange-traded-asset/GetCommonInformationExchangeTradedAssetNoBodyTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use TestDependencies\HTTP\HTTPResponseTest;
+
+final class GetCommonInformationExchangeTradedAssetNoBodyTest extends HTTPResponseTest {
+
+    public static function setUpBeforeClass(): void {
+
+        $Curl = curl_init();
+        $Url = self::getApiBase()."/common-information/exchange-traded-assets";
+        curl_setopt($Curl, CURLOPT_URL, $Url);
+        curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
+        self::$Response = curl_exec($Curl);
+        self::$HttpCode = curl_getinfo($Curl, CURLINFO_HTTP_CODE);
+
+    }
+
+    public function testResponseHttpCodeIs400(): void {
+
+        $this->assertSame(self::$HttpCode, 400);
+
+    }
+
+    public function testResponseDefaultExpectedValues(): void {
+
+        $ResponseArray = json_decode(self::$Response,true);
+        $this->assertSame($ResponseArray['error'], true);
+        $this->assertSame($ResponseArray['result'][0], 'Assets are required!');
+
+    }
+
+}
---
+++ tests/integration/common-information/exchange-traded-asset/GetCommonInformationExchangeTradedAssetValidAndInvalidTickerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use TestDependencies\HTTP\HTTPResponseTest;
+
+final class GetCommonInformationExchangeTradedAssetValidAndInvalidTickerTest extends HTTPResponseTest {
+
+    public static function setUpBeforeClass(): void {
+
+        $Curl = curl_init();
+        $Url = self::getApiBase()."/common-information/exchange-traded-assets";
+        curl_setopt($Curl, CURLOPT_URL, $Url);
+        curl_setopt($Curl, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+        curl_setopt($Curl, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($Curl, CURLOPT_POSTFIELDS, json_encode(['AAPL', 'InvalidTicker']));
+        curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
+        self::$Response = curl_exec($Curl);
+        self::$HttpCode = curl_getinfo($Curl, CURLINFO_HTTP_CODE);
+
+    }
+
+    public function testResponseHttpCodeIs200(): void {
+
+        $this->assertSame(self::$HttpCode, 200);
+
+    }
+
+    public function testResponseDefaultExpectedValues(): void {
+
+        $ResponseArray = json_decode(self::$Response,true);
+        $this->assertFalse(isset($ResponseArray['error']));
+        $this->assertTrue(isset($ResponseArray['result']['AAPL']));
+        $this->assertSame(count($ResponseArray['result']), 1);
+
+    }
+
+}
