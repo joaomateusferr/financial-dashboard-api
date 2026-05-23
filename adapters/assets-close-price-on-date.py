48a5997a41dc22ca0e6bec0d45dec36e637f2a94
Joao Ferreira <joaomateusferr@gmail.com>
2026-05-23 10:40:18 -0300

    Add file validations to assets close price on date adapter

--- adapters/assets-close-price-on-date.py
+++ adapters/assets-close-price-on-date.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import json
 from datetime import datetime
 from datetime import timedelta
@@ -5,33 +6,55 @@ import yfinance as yf
 import numpy as np
 import sys
 
-JsonContent = '{"2026-03-27":["BCIA11.SA"],"2026-04-01":["BBDC4.SA"],"2026-04-13":["B3SA3.SA"],"2026-04-15":["HGBS11.SA","HGLG11.SA","KNRI11.SA","NDIV11.SA","XPLG11.SA"],"2026-04-20":["CPTS11.SA"],"2026-04-24":["ALZR11.SA","XPML11.SA"],"2026-04-30":["BBDC4.SA","BCIA11.SA"],"2026-05-04":["BBDC4.SA"]}'
-AssetsByDate = json.loads(JsonContent)
+try:
 
-Result = {}
+    FilePath = sys.argv[1] if len(sys.argv) > 1 and sys.argv[1] else None
 
-for Date, Assets in AssetsByDate.items() :
+    if not FilePath :
+        raise Exception("File path not specified!")
 
-    Date = datetime.strptime(Date, "%Y-%m-%d")
-    DateStart = Date.strftime("%Y-%m-%d")
-    DateEnd = Date + timedelta(days=1)
-    DateEnd = DateEnd.strftime("%Y-%m-%d")
+    FilePath = Path(FilePath)
 
-    Data = yf.download(Assets, start=DateStart, end=DateEnd, auto_adjust=True, progress=False)
-    Data = Data['Close'].to_numpy()[0]
+    if not FilePath.exists():
+       raise Exception("Path or file does not exist!")
 
-    Result[DateStart] = {}
+    if not FilePath.is_file():
+       raise Exception("Path does not lead to a file!")
 
-    if isinstance(Data, np.ndarray) :
+    if FilePath.suffix.lower() != '.json':
+        raise Exception("File is not json!")
 
-        for Index in range(len(Assets)):
+    Content = FilePath.read_text()
+    AssetsByDate = json.loads(Content)
 
-            Result[DateStart][Assets[Index]] = Data[Index]
+    Result = {}
 
-    else :
+    for Date, Assets in AssetsByDate.items() :
 
-        Result[DateStart][Assets[0]] = Data
+        Date = datetime.strptime(Date, "%Y-%m-%d")
+        DateStart = Date.strftime("%Y-%m-%d")
+        DateEnd = Date + timedelta(days=1)
+        DateEnd = DateEnd.strftime("%Y-%m-%d")
+
+        Data = yf.download(Assets, start=DateStart, end=DateEnd, auto_adjust=True, progress=False)
+        Data = Data['Close'].to_numpy()[0]
+
+        Result[DateStart] = {}
+
+        if isinstance(Data, np.ndarray) :
+
+            for Index in range(len(Assets)):
+
+                Result[DateStart][Assets[Index]] = Data[Index]
+
+        else :
+
+            Result[DateStart][Assets[0]] = Data
 
 
-print(json.dumps(Result))
+    print(json.dumps(Result))
 
+except Exception as ex:
+
+    print("Something went wrong ...\n"+ str(ex))
+    sys.exit(1)
